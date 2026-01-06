3d2dca6e43b4d8e7475d1c08359cd7874796fa0d
pcannon52 <prestoncannon52@gmail.com>
2026-01-05 23:38:25 -0800

    Start of generated swerve code for the 2025 B bot, added auto alignment subsystem

---
+++ src/main/java/frc/robot/subsystems/AutoAlignSubSytem.java
@@ -0,0 +1,118 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.units.Unit;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class AutoAlignSubSytem extends SubsystemBase {
+  /** Creates a new AutoAlignSubSytem. */
+  private final NetworkTable limelighTable;
+  
+  private final PIDController xController;
+  private final PIDController yController;
+  private final PIDController rotController;
+
+  private final CommandSwerveDrivetrain drivetrain;
+
+  final double POSITION_TOLERANCE = Units.inchesToMeters(.5);
+  final double ROTATION_TOLERANCE = Units.degreesToRadians(1);
+
+
+  public AutoAlignSubSytem(CommandSwerveDrivetrain drivetrain){
+    this.drivetrain = drivetrain;
+    this.limelighTable = NetworkTableInstance.getDefault().getTable("limelight1");
+
+     this.xController = new PIDController(2, 0, 1);
+     this.yController = new PIDController(2, 0, 1);
+    this.rotController = new PIDController(2, 0, 1);
+
+    this.rotController.enableContinuousInput(-180, 180);
+
+     // Set tolerances
+     this.xController.setTolerance(POSITION_TOLERANCE);
+     this.yController.setTolerance(POSITION_TOLERANCE);
+     this.rotController.setTolerance(ROTATION_TOLERANCE);
+
+
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+
+  public Command align_TO_M2Tag(Pose2d targetpose, int target){
+    return run(() -> {
+      double tv = limelighTable.getEntry("tv").getDouble(0);
+
+      if (tv == 1.0 && isMT2PoseReliable()) { // is MT2 pose is not defiened yet
+          Pose2d currentPose = drivetrain.getState().Pose;
+          
+          double xVel = xController.calculate(currentPose.getX(), targetpose.getX());
+          double yVel = yController.calculate(currentPose.getY(), targetpose.getY());
+          double rotVel = rotController.calculate(
+              currentPose.getRotation().getDegrees(), 
+              targetpose.getRotation().getDegrees()
+          );
+
+          drivetrain.setControl(fieldCentricDrive
+              .withVelocityX(xVel)
+              .withVelocityY(yVel)
+              .withRotationalRate(Math.toRadians(rotVel))
+          );
+      } else {
+          drivetrain.setControl(fieldCentricDrive
+              .withVelocityX(0)
+              .withVelocityY(0)
+              .withRotationalRate(0)
+          );
+      }
+  }).until(() -> isAlignedMegaTag(targetpose));
+}
+
+
+  
+
+  private boolean isAlignedMegaTag(Pose2d targetPose){
+    double tv = limelighTable.getEntry("tv").getDouble(0);
+
+      if (tv != 1.0) { // if target is not located
+        return false;
+    }
+
+        double[] botpose = getBotPoseBlue(); // not defined yet
+        
+        if (botpose.length >= 6) {
+            Pose2d currentPose = new Pose2d(
+                botpose[0],  // x component
+                botpose[1], //y component
+                Rotation2d.fromDegrees(botpose[5]) // yaw component
+            );
+            
+            double xError = Math.abs(targetPose.getX() - currentPose.getX());
+            double yError = Math.abs(targetPose.getY() - currentPose.getY());
+            double rotError = Math.abs(targetPose.getRotation().getDegrees() - currentPose.getRotation().getDegrees());
+            
+            return xError < POSITION_TOLERANCE && // this checks to see the if robot camara is within target 
+                   yError < POSITION_TOLERANCE && 
+                   rotError < ROTATION_TOLERANCE;
+        }
+        
+        return false;
+    }
+
+
+}
+
+
+
